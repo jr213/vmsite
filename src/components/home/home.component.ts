2b099b86cb7507789831b5c1750f13a985eb3a4c
Jake Schmidt <jakeschmidt51@gmail.com>
2023-05-09 15:19:40 -0700

    worked on the homepage

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  brands: any[] = [
+    { name: 'Grundfos', url: 'https://vanmarckeplumbingsupply.com/wp-content/uploads/2022/06/Grundfos-Logo-min.png' },
+    { name: 'Ruud', url: 'https://vanmarckeplumbingsupply.com/wp-content/uploads/2022/06/Rudd-Logo-min.png' },
+    { name: 'Bradford White', url: 'https://vanmarckeplumbingsupply.com/wp-content/uploads/2022/06/Bradford-White-Logo-1-min.png' },
+    { name: 'Uponor', url: 'https://vanmarckeplumbingsupply.com/wp-content/uploads/2022/06/uponor-logo-min.png' },
+    { name: 'Navien', url: 'https://vanmarckeplumbingsupply.com/wp-content/uploads/2022/06/Navien-Logo-min.png' },
+    { name: 'Mcdonald', url: 'https://vanmarckeplumbingsupply.com/wp-content/uploads/2022/06/McDonald-Logo-min.png' },
+    { name: 'American Standard', url: 'https://vanmarckeplumbingsupply.com/wp-content/uploads/2022/06/American-Standard-Logo-min.png' },
+    { name: 'Moen', url: 'https://vanmarckeplumbingsupply.com/wp-content/uploads/2022/06/Moen-Logo-min.png' },
+  ];
 }
